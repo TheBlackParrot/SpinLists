11662f42bb0c7d44310638b053bd173faa9297b2
TheBlackParrot <theblackparrot0@gmail.com>
2026-04-14 07:15:39 -0500

    add update notifs

---
+++ SpinLists/Classes/ReleaseVersion.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace SpinLists.Classes;
+
+internal class ReleaseVersion
+{
+    [JsonProperty("tag_name")] public string? Version { get; set; }
+    [JsonProperty("prerelease")] public bool IsPreRelease { get; set; }
+    
+    [JsonConstructor]
+    public ReleaseVersion() {}
+}
--- SpinLists/Plugin.cs
+++ SpinLists/Plugin.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using Newtonsoft.Json;
 using SpinCore.Translation;
+using SpinLists.Classes;
 using SpinLists.UI;
 using SpinShareLib;
+using UnityEngine;
 
 namespace SpinLists;
 
@@ -49,11 +55,77 @@ public partial class Plugin : BaseUnityPlugin
     private void OnEnable()
     {
         HarmonyInstance.PatchAll();
+        MainCamera.OnCurrentCameraChanged += CheckForUpdates;
+        
         SpinListPanel.CreateSpinListPanel();
     }
     public void OnDisable()
     {
         HarmonyInstance.UnpatchSelf();
+    }
+    
+    private static void CheckForUpdates(Camera _)
+    {
+        MainCamera.OnCurrentCameraChanged -= CheckForUpdates;
+        
+        Task.Run(async () =>
+        {
+            try
+            {
+                HttpClient httpClient = new();
+                httpClient.DefaultRequestHeaders.Add("User-Agent",
+                    $"{nameof(SpinLists)}/{MyPluginInfo.PLUGIN_VERSION} (https://github.com/TheBlackParrot/SpinLists)");
+                HttpResponseMessage responseMessage =
+                    await httpClient.GetAsync("https://api.github.com/repos/TheBlackParrot/SpinLists/releases/latest");
+                responseMessage.EnsureSuccessStatusCode();
+                string json = await responseMessage.Content.ReadAsStringAsync();
+
+                ReleaseVersion? releaseVersion = JsonConvert.DeserializeObject<ReleaseVersion>(json);
+                if (releaseVersion == null)
+                {
+                    Log.LogInfo("Could not get newest release version information");
+                    return;
+                }
+
+                if (releaseVersion.Version == null)
+                {
+                    Log.LogInfo("Could not get newest release version information");
+                    return;
+                }
+
+                if (releaseVersion.IsPreRelease)
+                {
+                    Log.LogInfo("Newest release version is a pre-release");
+                    return;
+                }
+
+                Version currentVersion = new(MyPluginInfo.PLUGIN_VERSION);
+                Version latestVersion = new(releaseVersion.Version);
+#if DEBUG
+                // just so we can see the notifications
+                if (currentVersion != latestVersion)
+#else
+            if (currentVersion < latestVersion)
+#endif
+                {
+                    Log.LogMessage(
+                        $"{nameof(SpinLists)} is out of date! (using v{currentVersion}, latest is v{latestVersion})");
+
+                    await Awaitable.MainThreadAsync();
+                    NotificationSystemGUI.AddMessage(
+                        $"<b>{nameof(SpinLists)}</b> has an update available! <alpha=#AA>(v{currentVersion} <alpha=#77>-> <alpha=#AA>v{latestVersion})\n<alpha=#FF><size=67%>See the shortcut button in the Mod Settings page to grab the latest update.",
+                        15f);
+                }
+                else
+                {
+                    Log.LogMessage($"{nameof(SpinLists)} is up to date!");
+                }
+            }
+            catch (Exception e)
+            {
+                Log.LogError(e);
+            }
+        });
     }
 
     public static void DebugMessage(string _)
